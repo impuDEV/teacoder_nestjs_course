64a328d1cffc7b10c7a737614563576208ac7412
Papernov Alexander <impudev@gmail.com>
2026-05-24 22:33:55 +1000

    nestjs through Prisma ORM

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,13 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Controller } from '@nestjs/common'
 import { ReviewService } from './review.service'
-import { CreateReviewDto } from './dto/create-review.dto'
 
 @Controller('reviews')
 export class ReviewController {
 	constructor(private readonly reviewService: ReviewService) {}
 
-	@Post()
-	create(@Body() dto: CreateReviewDto) {
-		return this.reviewService.create(dto)
-	}
+	// @Post()
+	// create(@Body() dto: CreateReviewDto) {
+	// 	return this.reviewService.create(dto)
+	// }
 }
